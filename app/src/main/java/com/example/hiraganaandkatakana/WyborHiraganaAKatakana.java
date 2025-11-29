7d6c99ebdec8a1bff5917f29b346c392749da34b
f4Mythical <grynka.adam@gmail.com>
2025-11-29 18:06:59 +0100

    Zrobione ze sie przesuwa po buttonach i Intenty dodane + custom background zrobiony

--- app/src/main/java/com/example/hiraganaandkatakana/WyborHiraganaAKatakana.java
+++ app/src/main/java/com/example/hiraganaandkatakana/WyborHiraganaAKatakana.java
@@ -11,23 +11,34 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-private Button buttonStart;
+public class WyborHiraganaAKatakana extends AppCompatActivity {
+private Button buttonKatakana;
+private Button buttonHiragana;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_wybor_hiragana_akatakana);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        buttonStart = findViewById(R.id.Poczatek);
-        buttonStart.setOnClickListener(new View.OnClickListener() {
+        buttonHiragana = findViewById(R.id.buttonHiragana);
+        buttonKatakana = findViewById(R.id.buttonKatakana);
+        buttonHiragana.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,WyborHiraganaAKatakana.class);
+                Intent intent = new Intent(WyborHiraganaAKatakana.this, WyborHiragana.class);
+                startActivity(intent);
+                finish();
+            }
+
+        });
+        buttonKatakana.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(WyborHiraganaAKatakana.this, WyborKatakana.class);
                 startActivity(intent);
                 finish();
             }
--- app/src/main/java/com/example/hiraganaandkatakana/WyborKatakana.java
+++ app/src/main/java/com/example/hiraganaandkatakana/WyborKatakana.java
@@ -1,9 +1,6 @@
 package com.example.hiraganaandkatakana;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,26 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-private Button buttonStart;
+public class WyborKatakana extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_wybor_katakana);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-        buttonStart = findViewById(R.id.Poczatek);
-        buttonStart.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,WyborHiraganaAKatakana.class);
-                startActivity(intent);
-                finish();
-            }
         });
     }
 }
